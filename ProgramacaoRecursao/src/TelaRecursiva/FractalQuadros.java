b2d554459f7442ae2f829588a63f1fe95036d92c
druideirabr <leo@2007>
2016-09-21 11:30:44 -0300

    codigo

--- ProgramacaoRecursao/src/TelaRecursiva/FractalQuadros.java
+++ ProgramacaoRecursao/src/TelaRecursiva/FractalQuadros.java
@@ -10,10 +10,11 @@ public class FractalQuadros {
 	private static void fazQuadro(Tela a, int dHoriz, int dVert, int x2, int y2) {
 
 		if (x2 > a.getCanvasWidth() / 32) {
-			fazQuadro(a, dHoriz + x2 / 2 + 1, dVert + 1, x2 / 2 - 1, y2 / 2 - 1);
-			fazQuadro(a, dHoriz + 1, dVert + y2 / 2 + 1, x2 / 2 - 1, y2 / 2 - 1);
 			a.desenhaRetangulo(dHoriz + x2 / 2, dVert, x2 / 2, y2 / 2);
 			a.desenhaRetangulo(dHoriz, dVert + y2 / 2, x2 / 2, y2 / 2);
+			fazQuadro(a, dHoriz + x2 / 2 + 1, dVert + 1, x2 / 2 - 1, y2 / 2 - 1);
+			fazQuadro(a, dHoriz + 1, dVert + y2 / 2 + 1, x2 / 2 - 1, y2 / 2 - 1);
+
 		}
 	}
 }
